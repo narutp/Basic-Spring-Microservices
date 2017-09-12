0ec7e43963a3550994cf01d2f4531ec593ceaf7b
Narut Poovorakit <net.narut.p@gmail.com>
2017-09-12 16:14:08 +0700

    Add buefy table

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
+    <!--  Font awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!--  Font for buefy -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
     <navbar></navbar>
     <router-view></router-view>
     <footerbar></footerbar>
--- frontend/frontend/src/components/DocumentTable.vue
+++ frontend/frontend/src/components/DocumentTable.vue
@@ -1,27 +1,41 @@
 <template lang="html">
   <div document-table--container>
     <hr>
-    <!-- <table class="table is-striped is-fullwidth" style="margin: 30px">
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Title</th>
-          <th>Writer</th>
-          <th>Create Date</th>
-          <th>Updated Date</th>
-          <th>Option</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="row in people">
-          <td>{{row.id}}</td>
-          <td>{{row.title}}</td>
-          <td>{{row.writer}}</td>
-          <td>{{row.date}}</td>
-          <td>{{row.updateDate}}</td>
-        </tr>
-      </tbody>
-    </table> -->
+    <section>
+      <b-table
+          :data="tableData"
+          :paginated="isPaginated"
+          :per-page="5"
+          default-sort="user.first_name">
+
+          <template scope="props">
+              <b-table-column field="id" label="ID" width="40" sortable numeric>
+                  {{ props.row.id }}
+              </b-table-column>
+
+              <b-table-column field="user.first_name" label="First Name" sortable>
+                  {{ props.row.user.first_name }}
+              </b-table-column>
+
+              <b-table-column field="user.last_name" label="Last Name" sortable>
+                  {{ props.row.user.last_name }}
+              </b-table-column>
+
+              <b-table-column field="date" label="Date" sortable centered>
+                  <span class="tag is-success">
+                      {{ new Date(props.row.date).toLocaleDateString() }}
+                  </span>
+              </b-table-column>
+              
+              <b-table-column label="Gender">
+                  <b-icon pack="fa"
+                      :icon="props.row.gender === 'Male' ? 'mars' : 'venus'">
+                  </b-icon>
+                  {{ props.row.gender }}
+              </b-table-column>
+          </template>
+      </b-table>
+    </section>
   </div>
 </template>
 
@@ -29,11 +43,16 @@
 export default {
   data () {
     return {
-      people: [
-          {id: 1, title: 'Bob AB', writer: 'Boogey', date: '28/07', updateDate: '09/10'},
-          {id: 2, title: 'ADDD', writer: 'Adam', date: '30/07'},
-          {id: 3, title: 'XSACC', writer: 'Helao', date: '28/07'}
-      ]
+      tableData: [{'id': 1, 'user': {'first_name': 'Jesse', 'last_name': 'Simmons'}, 'date': '2016-10-15 13:43:27', 'gender': 'Male'},
+      {'id': 2, 'user': {'first_name': 'John', 'last_name': 'Jacobs'}, 'date': '2016-12-15 06:00:53', 'gender': 'Male'},
+      {'id': 3, 'user': {'first_name': 'John', 'last_name': 'Jacobs'}, 'date': '2016-12-15 06:00:53', 'gender': 'Male'},
+      {'id': 4, 'user': {'first_name': 'John', 'last_name': 'Jacobs'}, 'date': '2016-12-15 06:00:53', 'gender': 'Male'},
+      {'id': 5, 'user': {'first_name': 'John', 'last_name': 'Jacobs'}, 'date': '2016-12-15 06:00:53', 'gender': 'Male'},
+      {'id': 6, 'user': {'first_name': 'John', 'last_name': 'Jacobs'}, 'date': '2016-12-15 06:00:53', 'gender': 'Male'}],
+      isPaginated: true,
+      isPaginationSimple: false,
+      defaultSortDirection: 'asc',
+      perPage: 5
     }
   }
 }
