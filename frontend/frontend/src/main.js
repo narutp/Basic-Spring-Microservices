b659c0eb0e940feeb5fbb26dc78662f00d7b88f5
Narut Poovorakit <net.narut.p@gmail.com>
2017-09-12 15:39:57 +0700

    Add buefy.js

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import bulma from 'bulma/css/bulma.css'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 Vue.use(bulma)
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
